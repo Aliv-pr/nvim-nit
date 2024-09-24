5f0c020f88a11ebb702a54e34d071964223392a8
AlexPRIvan <alex2408rosales@gmail.com>
2024-09-23 20:50:27 -0600

    Hacemos una llamda en init.lua para luego pasarle desde el archivo plugins.lua nuestros complementos

--- init.lua
+++ init.lua
@@ -22,32 +22,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", 
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000 
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    }
-  },
-
-}
-local opts = {}
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
 -- Configuración de neo-tree
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
